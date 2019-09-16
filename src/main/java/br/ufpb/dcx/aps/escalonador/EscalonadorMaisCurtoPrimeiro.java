127996c1debfef187ce8a015ff518db7c0b51af1
CaioHenrique5 <Caio.lima@dce.ufpb.br>
2019-09-16 09:35:37 -0300

    Criação da classe Status e dos Construtores do Escalonador Mais Curto

---
+++ src/main/java/br/ufpb/dcx/aps/escalonador/EscalonadorMaisCurtoPrimeiro.java
@@ -0,0 +1,18 @@
+package br.ufpb.dcx.aps.escalonador;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class EscalonadorMaisCurtoPrimeiro extends Escalonador {
+
+	StatusEscalonadorMCP status = new StatusEscalonadorMCP();
+
+	private int tick;
+	private List<String> fila = new ArrayList<>();
+	private List<Integer> duracoes = new ArrayList<>();
+
+	private String processoRodando;
+
+	private int duracaoFixa = 0;
+	private int duracaoRodando = 0;
+}
